badca458cdd9ebb59b66f1b16cc576fd09d5c438
Kelly Villalobos <kvdesign1978@gmail.com>
2018-06-19 14:43:14 -0500

    views exercise with dice roll, partials and posts

--- src/main/java/com/codeup/blog/PostController.java
+++ src/main/java/com/codeup/blog/PostController.java
@@ -1,41 +1,62 @@
 package com.codeup.blog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class PostController {
-@GetMapping("/posts")
-    public @ResponseBody String Index(){
-    //make an arraylist of post objects to send to the view.
-    return"Here are all the Ads!";
+    @GetMapping("/posts")
+    public String Index(Model model) {
+        //make an arraylist of post objects to send to the view.
+        List<Post> posts = new ArrayList<>();
+        Post post1 = new Post(
+                "Title form array of posts", "This is the first post of the array", 1
+        );
+        Post post2 = new Post(
+                "Title from array of posts", "This is the second post of the array", 2
+        );
+        posts.add(post1);
+        posts.add(post2);
+        model.addAttribute("posts", posts);
 
-}
+        return "/posts/index";
 
-    @GetMapping("/posts/{id}")
-    public @ResponseBody String ShowDetails(@PathVariable long id){
-        return "Viewing post #" + id;
+    }
+
+    @GetMapping("/post/{id}")
+    public String ShowDetails(@PathVariable long id, Model view) {
+        Post post = new Post("This is a single post", "This is the post body", id);
+        view.addAttribute("post", post);
+        return "/posts/show";
 
 
     }
 
     @GetMapping("/posts/{id}/edit")
-    public @ResponseBody String Edit(@PathVariable long id){
-        return"View the form for editing post #" + id;
+    public @ResponseBody
+    String Edit(@PathVariable long id) {
+        return "View the form for editing post #" + id;
 
     }
+//
+//    @GetMapping("/posts/create")
+//    public
+//    String Create(Model view) {
+//
+//        return "/posts/create";
+//
+//    }
 
-    @GetMapping("/posts/create")
-    public @ResponseBody String Create(){
-        return"View the form for creating a post";
-
-    }
-
-    @GetMapping("Post/create")
-    public  @ResponseBody String savePost(){
-    return "saving to the database";
+    @GetMapping("com.codeup.blog.Post/create")
+    public @ResponseBody
+    String savePost() {
+        return "saving to the database";
     }
 
 }
